f41177cd9ad2afe0433aa21a6cd8691872502a32
houjie <houjie@smartdevices.com.cn>
2019-04-05 16:52:43 +0800

    update protoc version

--- setup.sh
+++ setup.sh
@@ -8,7 +8,11 @@ cd ./protobuf_javanano/JavaNanoTest
 export TEST_NANO_BIN_PATH=`pwd`/test.bin
 echo "TEST_NANO_BIN_PATH:" $TEST_NANO_BIN_PATH
 cd -
-cd ./protobuf_nano_c/nanopb-0.3.4-linux-x86/generator-bin
+if [ "$(uname)"="Darwin" ];then
+    cd ./protobuf_nano_c/nanopb-0.3.9.3-macosx-x86/generator-bin
+elif [ "$(expr substr $(uname -s) 1 5)"="Linux" ];then
+    cd ./protobuf_nano_c/nanopb-0.3.9.3-linux-x86/generator-bin
+fi
 export PROTC_PATH=`pwd`
 echo "PROTC_PATH:" $PROTC_PATH
 cd -
